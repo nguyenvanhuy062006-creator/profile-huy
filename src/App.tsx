35b5ad1143ceee7be15f20e2e7bd8b31e8e17e65
Macbook <macbook@MacBook-Cua-Huy.local>
2026-06-16 14:03:04 +0700

    feat: ngày 1 git push origin dev

--- src/App.tsx
+++ src/App.tsx
@@ -1,122 +1,108 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+interface MultiLang{
+  vi: string;
+  en:string;
+}
+interface Project{
+  id: number;
+  title: MultiLang;
+  description: MultiLang;
+  technologies: string[];
+}
+interface Skill{
+  name: string;
+  level: string;
+}
+interface Profile{
+  name:string;
+  title: MultiLang;
+  aboutMe: MultiLang;
+  skills: Skill[];
+  projects: Project[];
+}
+const profileData: Profile={
+  name:'Nguyễn Văn Huy',
+  title:{
+    vi: "Thực tập sinh Front-end",
+    en: "Intern Front-end",
+  },
+  aboutMe:{
+    vi: "Xin chào! Tôi là Huy, hiện tại đang là sinh viên tại Trường Đại học Điện Lực. Mình có đam mê xây dựng trang web tối ưu, giao diện thân thiện và dễ tiếp cận.",
+    en: "Hello! I am Huy, currently an IT student at Electric Power University. I am passionate about building optimized web applications with friendly and accessible user interfaces."
+  },
+  skills: [
+    {name: "HTML", level: "Beginner"},
+    {name: "CSS", level: "Beginner"},
+    {name: "Javascript", level: "Beginner"},
+  ],
+  projects: [{
+    id:1,
+    title: {
+      vi: "Hệ thống bán xe oto online", en: "Online car sales system"},
+    description:{
+      vi:"Hệ thống trang tin tức, khuyến mãi, hiện thông báo người dùng đăng kí khuyến mãi.",
+      en:"News and promotion system that displays notifications when users register for promotions.",
+    },
+    technologies: ["HTML","CSS","JAVASCRIPT"]
+  },
+]
+};
+export default function App(){
+  const[lang, setLang]=useState<'vi' | 'en'>('vi');
   return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
+    <div className='container'>
+      {/*Header*/}
+      <header className='header'>
+        <div className="header-content">
+          <span className="logo">Huy profile</span>
+          <button onClick={()=>setLang(lang==='vi' ? 'en':"vi")} className='lang-btn'>
+            {lang ==='vi' ?"Switch to EN":"Chuyển sang VI"}
+          </button>
         </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
+      </header>
+      {/*Main */}
+      <main className="main-content">
+        {/*about me*/}
+        <section className="profile-card">
+          <h1 className="prf-name">{profileData.name}</h1>
+          <p className='prf-title'>{profileData.title[lang]}</p>
+          <p className='prf-about'>{profileData.aboutMe[lang]}</p>
+        </section>
+        <section>
+          <h2 className='section-title'>{lang ==='vi'?'Kỹ năng của tôi': 'My skill'}</h2>
+          <div className='skill-grid'>{profileData.skills.map((skill,index)=>(
+            <div key={index} className='skill-item'>
+              <p className="skill-name">{skill.name}</p>
+              <span className='skill-level'>{skill.level}</span>
+            </div>
+          ))}
+            </div>
+        </section>
+        <section>
+          <h2 className="section-title">{lang === 'vi' ? 'Dự án tiêu biểu' : 'Featured Projects'}</h2>
+          <div className="projects-grid">
+            {profileData.projects.map((project) => (
+              <div key={project.id} className="project-card">
+                <div className="project-info">
+                  <h3 className="project-title">{project.title[lang]}</h3>
+                  <p className="project-desc">{project.description[lang]}</p>
+                </div>
+                <div className="tech-tags">
+                  {project.technologies.map((tech, index) => (
+                    <span key={index} className="tech-tag">{tech}</span>
+                  ))}
+                </div>
+              </div>
+            ))}
+          </div>
+        </section>
 
-      <div className="ticks"></div>
 
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
 
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
+      </main>
+
+
+    </div>
   )
 }
-
-export default App
